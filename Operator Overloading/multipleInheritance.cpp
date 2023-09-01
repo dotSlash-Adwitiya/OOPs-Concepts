1984fea93dd121f751ff0275845f3a2c97a2e679
Adwitiya Mourya <adwitiya.code@gmail.com>
2023-09-01 16:22:45 +0530

    Multiple Inheritance solution with ::

---
+++ Operator Overloading/multipleInheritance.cpp
@@ -0,0 +1,31 @@
+
+#include<iostream>
+using namespace std;
+
+ 
+class Base1 {
+  public : 
+    void show() {
+      cout << "Show() of BASE-1" << endl;
+    }
+};
+
+class Base2 {
+  public : 
+    void show() {
+      cout << "Show() of BASE-2" << endl;
+    }
+};
+
+class Derived : public Base1, public Base2 {};
+
+int main()  {
+  Derived d;
+
+  //* Causes Ambiguity to compiler -which show() to call.
+  // d.show();
+
+  //* One possible Solution : Use scope resolution operator 
+  d.Base1::show();
+  d.Base2::show();
+}
