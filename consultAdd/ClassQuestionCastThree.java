9a27c8ae921efe2f2d53a25e420ddf8219f43046
Adwitiya Mourya <adwitiya.code@gmail.com>
2023-09-12 15:42:44 +0530

    Added valid typecasting code - (Child Class to Parent Class)

--- consultAdd/ClassQuestionCastThree.java
+++ consultAdd/ClassQuestionCastThree.java
@@ -1,14 +1,23 @@
 class A {
-    //* ---->>> Every String is object but not every OBject is a STRING, it could be anything. -- <<<<<
-    Object fun2(Object obj) {
-        //* Code Won't work because of below line, and throws - ClassCastException */
-        return (String) obj;
+    
+    //* This below code, won't give any error */
+    //* Because, a child class can be typecasted into its parent class */
+    //* But vice-versa isn't true ! */
+    Object fun1(String str) {
+        return (Object) str+"Adwitiya";
     }
+    //* O/p : Adwitiya */
+
+    //* ---->>> Every String is object but not every OBject is a STRING, it could be anything. -- <<<<<
+    // Object fun2(Object obj) {
+    //     //* Code Won't work because of below line, and throws - ClassCastException */
+    //     return (String) obj;
+    // }
 }
 
 public class ClassQuestionCastThree {
   public static void main(String[] args) {
     A a = new A();
-    System.out.println(a.fun2(new Object()));
+    System.out.println(a.fun1(new String()));
 }
 }
