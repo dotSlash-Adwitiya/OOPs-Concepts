dcff5245c9f78aa45a82548192932e87043796ff
Adwitiya Mourya <adwitiya.code@gmail.com>
2024-04-23 19:43:43 +0530

    Capitalized P

--- Tricky C++ Questions/voidPtr.cpp
+++ Tricky C++ Questions/voidPtr.cpp
@@ -6,7 +6,7 @@ int main()
   int b = 5;
   void *a = &b; // * Void is a generic pointer type 
   
-  // * Void ptr needs to be explicityly typecaseted into a valid datatype because its a generic type
+  // * Void Ptr needs to be explicityly typecaseted into a valid datatype because its a generic type
   // * And hence the compiler doesn't have enough information thats why it will give compiler error
   // * Hence we need to first convert it into appropriate ptr type -> then datatype (int).
   cout << "Address of B: " << &b << " Address of A: " << *(int*) a << endl;
